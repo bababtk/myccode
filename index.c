7f5c26c459b6ee5dc992cf59ff8abda9b34a51a7
keshav rathi <keshavrathi21@yahoo.in>
2018-09-21 14:56:35 +0530

    changed

--- index.c
+++ index.c
@@ -1,6 +1,26 @@
 #include<stdio.h>
 
-main(){
-printf("hello world");
+
+ main(){
+  int arr[20],n,res;
+  printf("enter number of elements u want to find the sum of");
+  scanf("%d",&n);
+  res=array(arr,n);
+  printf("the sum is %d ",res);
 }
 
+int array(int a[20], int m)
+{
+  int i,sum=0;
+  printf("enter array elements");
+  for(i=0;i<m;i++)
+  {scanf("%d",&a[i]);
+  }
+  
+  for(i=0;i<m;i++)
+  {
+    sum=sum+a[i];
+  }
+  
+  return sum;
+}
